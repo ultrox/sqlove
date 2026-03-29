41f9f65191f9df4e894af52cdb2906123a28b846
Marko Vujanic <crashxx@gmail.com>
2026-03-29 17:22:35 +0200

    add coverage with vitest/v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["test/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*.ts"],
+      exclude: ["src/cli.ts", "src/main.ts"],
+      reporter: ["text", "lcov"],
+    },
   },
 });
